286f59a24061d57faad734f449c1629e3b0c689f
Harsha Sree <harshasree2809@gmail.com>
2026-03-20 10:31:49 +0530

    Update logo import path to use uppercase filename

--- src/compoents/nav.jsx
+++ src/compoents/nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jfif";
+import logo from "../assets/LOGO.jfif";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
